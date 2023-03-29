6e93bac72b03623bd59c48ee698934511e88c7e9
DeviousWings <nksteele@byui.edu>
2023-03-28 21:50:25 -0500

    first commit

---
+++ client/src/pages/Signup.tsx
@@ -0,0 +1,11 @@
+export function Signup() {
+    return (
+        <>
+    <h1 className="text-3x1 font-bold mb-8 text-center">Signup</h1>
+    <form className="grid gird-cols-[auto,1fr] gap-x-3 gap-y-5 items-center justify-items-end">
+        <label>Username</label>
+        <input />
+    </form>
+    </>
+    )
+}
